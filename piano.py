0fe071e8206e0c6b191c0670ac9c549b71454c7b
gmittal <gautam@mittal.net>
2016-06-22 13:50:20 -0700

    basic synth setup with piano.py and scikit audiolab

--- piano.py
+++ piano.py
@@ -1,13 +1,34 @@
 from __future__ import division
+import math
 import numpy as np
 from scikits.audiolab import play
 
-def testsignal(hz,seconds, amplitude = .5,sr=44100.):
-    '''
-    Create a sine wave at hz for n seconds
-    '''
-    # cycles per sample
+bpm = 160
+noteFrequencies = []
+noteNamesWithSharps = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+noteNamesWithFlats = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
+referenceFrequency = referenceFrequency = 130.81278265*3 # C
+
+for i in range(0, 12):
+    noteFrequencies.append(referenceFrequency*math.pow(2, i/12))
+
+def sig(hz,seconds, amplitude = .5,sr=44100.):
     cps = hz / sr
-    # total samples
     ts = seconds * sr
     return amplitude * np.sin(np.arange(0,ts*cps,cps) * (2*np.pi))
+
+# sync playNote function using frequency lookup
+def playNote(note, beats):
+    seconds = beats*60/bpm
+    freq = 800
+    if (note in noteNamesWithSharps):
+        freq = noteFrequencies[noteNamesWithSharps.index(note)]
+    else:
+        freq = noteFrequencies[noteNamesWithFlats.index(note)]
+    s = sig(freq, seconds)
+    play(s)
+
+playNote("C", 1/2)
+playNote("C#", 1/2)
+playNote("D", 1)
+playNote("G", 4)
