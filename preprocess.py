5a635fc87bfaad0e1e926caa6da269a52450de4c
gmittal <gautam@mittal.net>
2016-08-12 08:06:16 -0700

    Finished basic data preprocessing script.

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,6 @@
+# Simple script to turn JSON data into a numpy array for use with RNN
+# $ python preprocess.py /path/to/data.json datasetName
+
 import argparse
 import os
 import numpy as np
@@ -16,12 +19,21 @@ def lst_set(lst):
 # Get command line arguments
 parser = argparse.ArgumentParser(description='Data: JSON to Numpy Array Preprocesser.')
 parser.add_argument('data', action="store", type=str)
+parser.add_argument('dataset_name', action="store", type=str)
+datasetName = parser.parse_args().dataset_name
 
 # Grab the JSON file contents
 filePath = os.getcwd() + "/" + parser.parse_args().data
 with open(filePath) as data_file:
     jsonFile = json.load(data_file)
 
-# Declare data points and vocabulary (set of data points, no duplicates)
+# Declare data points and assign index
 data = jsonFile["data"]
 vocab = list(lst_set(data))
+data_ix = []
+for index in range(0, len(data)):
+    data_ix.append(vocab.index(data[index]))
+
+os.makedirs(os.getcwd()+"/data/datasets/preprocessed/"+datasetName)
+np.savetxt(os.getcwd()+"/data/datasets/preprocessed/"+datasetName+"/vocab.gz", vocab)
+np.savetxt(os.getcwd()+"/data/datasets/preprocessed/"+datasetName+"/dataset.gz", data_ix)
